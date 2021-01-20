ede5bb24d3b07c066e2d81b5e2b900bfbd2fb026
Mateusz Trybuła <zerq1998@gmail.com>
2021-01-20 22:59:10 +0100

    Add waveform Add time slider

---
+++ AudioPlayerApp/AudioFeatures/OwnWaveform.cs
@@ -0,0 +1,46 @@
+﻿using AudioPlayerApp.Converter;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace AudioPlayerApp.AudioFeatures
+{
+    public class OwnWaveform
+    {
+        public BitmapImage DrawWaveform(List<byte> wave)
+        {
+            Color areaColor = Color.FromArgb(12,255,0);
+            Color edgeColor = Color.FromArgb(255, 0, 0);
+            Color backGroundColor = Color.FromArgb(118, 118, 118);
+            Rectangle bounds = Rectangle.FromLTRB(0, 0, 850, 300);
+            float width = bounds.Width;
+            float height = bounds.Height;
+            Bitmap bmp = new Bitmap(800,300);
+
+            using(Graphics g = Graphics.FromImage(bmp))
+            {
+                Pen edgePen = new Pen(edgeColor);
+                edgePen.LineJoin = System.Drawing.Drawing2D.LineJoin.Round;
+                edgePen.Width = 1;
+                Brush areaBrush = new SolidBrush(areaColor);
+
+                List<Point> points = new List<Point>();
+
+                foreach(var bt in wave)
+                {
+                    points.Add(new Point(bt));
+                }
+
+                g.FillClosedCurve(areaBrush, points.ToArray());
+                g.DrawClosedCurve(edgePen, points.ToArray(),0.1f,FillMode.Winding);
+            }
+
+            return BitmapConverter.ConvertToBitmapImage(bmp);
+        }
+    }
+}
--- AudioPlayerApp/MainWindow.xaml.cs
+++ AudioPlayerApp/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Media;
@@ -16,6 +18,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace AudioPlayerApp
 {
@@ -24,8 +27,10 @@ namespace AudioPlayerApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<float> wave;
         MediaPlayer player;
+        DispatcherTimer timer;
+        private string maxTime;
+        private bool isPlaying=false;
 
         public MainWindow()
         {
@@ -33,37 +38,97 @@ namespace AudioPlayerApp
             this.CloseBtn.Click += (s, e) => this.Close();
             this.MinimizeBtn.Click += (s, e) => this.WindowState = WindowState.Minimized;
             player = new MediaPlayer();
+            DurationText.Text = "00:00/00:00";
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            var actual = player.Position.ToString(@"mm\:ss");
+            DurationSlider.Value = player.Position.TotalSeconds;
+            DurationText.Text = actual + "/" + maxTime;
         }
 
         private void LoadWave(string name)
         {
             try
             {
-                byte[] waveBytes = File.ReadAllBytes(name);
-                wave = new List<float>();
-                for (int i = 0; i < waveBytes.Length; i++)
-                {
-                    wave.Add(waveBytes[i]);
-                }
-
-                SpectrumImage.Source = new Waveform().DrawWaveform(wave);
+                SpectrumImage.Source = new Waveform().DrawWaveform(name);
             }
             catch(Exception ex)
             {
-                MessageBox.Show("Something went wrong!!");
+                MessageBox.Show("Something went wrong with drawing line!!");
             }
         }
 
         private void LoadWavBtn_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog fileDialog = new OpenFileDialog();
-            fileDialog.Filter = "Wav files (*.wav) | *.wav";
+            fileDialog.Filter = "Audio files (*.wav,*.mp3) | *.wav;*.mp3";
             if (fileDialog.ShowDialog() == true)
             {
                 var name = fileDialog.FileName;
-                LoadWave(name);
                 player.Open(new Uri(name));
-                player.Play();
+                Duration duration = player.NaturalDuration;
+                if (duration.HasTimeSpan)
+                {
+                    LoadWave(name);
+                    DurationSlider.Maximum = duration.TimeSpan.TotalSeconds;
+                    maxTime = duration.TimeSpan.ToString(@"mm\:ss");
+                    timer.Start();
+                    player.Play();
+                    isPlaying = true;
+                    PlayPauseImg.Source = new BitmapImage(new Uri("pack://application:,,,/Graphics/pause.png"));
+                }
+                else
+                {
+                    MessageBox.Show("There was an error during song loading !!");
+                }
+            }
+        }
+
+        private void DurationSlider_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            if (isPlaying)
+            {
+                player.Position = TimeSpan.FromSeconds(DurationSlider.Value);
+            }
+        }
+
+        private void StepBackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (isPlaying)
+            {
+                player.Position -= TimeSpan.FromSeconds(10);
+            }
+        }
+
+        private void PlayPauseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (player!=null)
+            {
+                if (isPlaying)
+                {
+                    player.Pause();
+                    PlayPauseImg.Source = new BitmapImage(new Uri("pack://application:,,,/Graphics/play.png"));
+                }
+                else
+                {
+                    player.Play();
+                    PlayPauseImg.Source = new BitmapImage(new Uri("pack://application:,,,/Graphics/pause.png"));
+                }
+
+                isPlaying = !isPlaying;
+            }
+        }
+
+        private void ForwardBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (isPlaying)
+            {
+                player.Position += TimeSpan.FromSeconds(10);
             }
         }
     }
